019d97e3ed8ec995e82eba9b08b5ae8e77471ce1
Gabriel Lima <102686294+GabrielLima07@users.noreply.github.com>
2024-06-11 12:49:30 -0300

    feat: ticket modal

--- src/components/DepartmentTicketsCard/Card.jsx
+++ src/components/DepartmentTicketsCard/Card.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
-import "./card.css"
+import React, { useEffect, useState } from 'react';
+import './card.css';
+import getCustomerByTicketId from './../../services/ticket/getCustomerByTicketId';
+import Modal from './../DepartmentTicketsModal/Modal';
 
 const Card = ({ ticket }) => {
+  const [customerName, setCustomerName] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchCustomerName = async () => {
+      const customer = await getCustomerByTicketId(ticket.id);
+      if (customer) {
+        setCustomerName(customer.name);
+      }
+    };
+
+    fetchCustomerName();
+  }, [ticket.id]);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   if (!ticket) {
     return <div>Loading...</div>; 
   }
 
   return (
-    <div id='card-main-div'>
-      <div id='card-flex-container'>
-        <div id='card-ticket-data'>
-          <p id='card-ticket-description'>{ticket.description}</p>
-          <p id='card-ticket-id'>{ticket.id}</p>
-          <p id='card-ticket-opened'>Aberto dia {ticket.date} por ~nome~</p>
+    <div>
+      <div id='card-main-div' onClick={openModal}>
+        <div id='card-flex-container'>
+          <div id='card-ticket-data'>
+            <p id='card-ticket-description'>{ticket.title}</p>
+            <p id='card-ticket-id'>{ticket.id}</p>
+            <p id='card-ticket-opened'>Aberto dia {ticket.date} por {customerName}</p>
+          </div>
+          {/* <div id='card-emplyee-picture'>
+            pic
+          </div> */}
         </div>
-        {/* <div id='card-emplyee-picture'>
-          pic
-        </div> */}
       </div>
+      <Modal 
+        isOpen={isModalOpen} 
+        onClose={closeModal} 
+        ticket={ticket} 
+        customerName={customerName}
+      />
     </div>
   )
 }
 
-export default Card
+export default Card;
--- src/components/DepartmentTicketsMainContent/MainContent.jsx
+++ src/components/DepartmentTicketsMainContent/MainContent.jsx
@@ -10,41 +10,49 @@ const MainContent = () => {
     const fetchDepartmentTickets = async () => {
       const data = await getDepartmentTickets();
       setTickets(data);
-      console.log(tickets);
+      console.log(data);
     }
 
     fetchDepartmentTickets();
   }, []);
 
   return (
-    <div>
+    <div id="dept-ticket-main-div">
       <div id="dept-tickets-main-content">
-        <section className='dept-tickets-list'>
+        <section className='dept-tickets'>
           <h1>Tickets abertos</h1>
-          {tickets.map((ticket) => {
-            return ticket.status === "Pendente" ? <Card ticket={ticket} key={ticket.id} /> : null;
-          })}
+          <div className='dept-tickets-list'>
+            {tickets.map((ticket) => {
+              return ticket.status === "Pendente" ? <Card ticket={ticket} key={ticket.id} /> : null;
+            })}
+          </div>
         </section>
 
-        <section className='dept-tickets-list'>
+        <section className='dept-tickets'>
           <h1>Tickets em atendimento</h1>
-          {tickets.map((ticket) => {
-            return ticket.status === "Em atendimento" ? <Card ticket={ticket} key={ticket.id} /> : null;
-          })}
+          <div className='dept-tickets-list'>
+            {tickets.map((ticket) => {
+              return ticket.status === "Em atendimento" ? <Card ticket={ticket} key={ticket.id} /> : null;
+            })}
+          </div>
         </section>
 
-        <section className='dept-tickets-list'>
+        <section className='dept-tickets'>
           <h1>Tickets em impedimento</h1>
-          {tickets.map((ticket) => {
-            return ticket.status === "Em impedimento" ? <Card ticket={ticket} key={ticket.id} /> : null;
-          })}
+          <div className='dept-tickets-list'>
+            {tickets.map((ticket) => {
+              return ticket.status === "Em impedimento" ? <Card ticket={ticket} key={ticket.id} /> : null;
+            })}
+          </div>
         </section>
 
-        <section className='dept-tickets-list'>
+        <section className='dept-tickets'>
           <h1>Tickets fechados</h1>
-          {tickets.map((ticket) => {
-            return ticket.status === "Fechado" ? <Card ticket={ticket} key={ticket.id} /> : null;
-          })}
+          <div className='dept-tickets-list'>
+            {tickets.map((ticket) => {
+              return ticket.status === "Fechado" ? <Card ticket={ticket} key={ticket.id} /> : null;
+            })}
+          </div>
         </section>
       </div>
     </div>
