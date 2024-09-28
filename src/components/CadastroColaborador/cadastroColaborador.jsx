54600ed68c862c0b2142ce109409c7fc65090305
Giovanna Cunha <giovanna.00000847950@unicap.br>
2024-09-28 15:29:22 -0300

    adicionando rotas e modificando estilizações

--- src/components/CadastroColaborador/cadastroColaborador.jsx
+++ src/components/CadastroColaborador/cadastroColaborador.jsx
@@ -1,44 +1,81 @@
 import React, { useState } from "react";
-import NavbarAdm from "../NavbarAdm/navbarAdm";
+import Navbar from "../NavbarAdm2.jsx/MenuAdm"
 import { isEquals } from './../../utils/isEquals';
 import signUpEmployee from './../../services/auth/signUpEmployee';
+import signUpCustomer from "../../services/auth/signUpCustomer";
+
+import twitterLogo from "../../assets/Home/twitter.png";
+import instagramLogo from "../../assets/Home/instagram.png";
+import facebookLogo from "../../assets/Home/facebook.png";
 
 const cadastroColaborador = () => {
 
-    const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const [senhaRepetida, setSenhaRepetida] = useState('');
+    //Cadastro dos funcionários
+    const [nomeColaborador, setNomeColaborador] = useState('');
+    const [emailColaborador, setEmailColaborador] = useState('');
+    const [senhaColaborador, setSenhaColaborador] = useState('');
+    const [senhaRepetidaColaborador, setSenhaRepetidaColaborador] = useState('');
     const [departamento, setDepartament] = useState('');
 
-    const handleNomeChange = (event) => {
-        setNome(event.target.value);
+    //Cadastro dos clientes
+    const [nomeCliente, setNomeCliente] = useState('');
+    const [emailCliente, setEmailCliente] = useState('');
+    const [senhaCliente, setSenhaCliente] = useState('');
+    const [telefoneCliente, setTelefoneCliente] = useState('');
+    const [senhaRepetidaCliente, setSenhaRepetidaCliente] = useState('');
+    
+
+    //Funções para funcionário
+    const handleNomeColaboradorChange = (event) => {
+        setNomeColaborador(event.target.value);
     };
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
+    const handleEmailColaboradorChange = (event) => {
+        setEmailColaborador(event.target.value);
     };
 
-    const handleSenhaChange = (event) => {
-        setSenha(event.target.value);
+    const handleSenhaColaboradorChange = (event) => {
+        setSenhaColaborador(event.target.value);
     };
 
-    const handleSenhaRepetidaChange = (event) => {
-        setSenhaRepetida(event.target.value);
+    const handleSenhaRepetidaColaboradorChange = (event) => {
+        setSenhaRepetidaColaborador(event.target.value);
     };
 
     const handleDepartamentoChange = (event) => {
         setDepartament(event.target.value);
     }
 
+    //Funções para cliente
+    const handleNomeClienteChange = (event) => {
+        setNomeCliente(event.target.value);
+    };
+
+    const handleEmailClienteChange = (event) => {
+        setEmailCliente(event.target.value);
+    };
+
+    const handleTelefoneClienteChange = (event) => {
+        setTelefoneCliente(event.target.value);
+    }
+
+    const handleSenhaClienteChange = (event) => {
+        setSenhaCliente(event.target.value);
+    };
+
+    const handleSenhaRepetidaClienteChange = (event) => {
+        setSenhaRepetidaCliente(event.target.value);
+    };
+
+    //Função para o botão de cadastro do funcionário
     const handleCadastroColaboradorClick = async () => {
-        if (isEquals(senha, senhaRepetida)) {
+        if (isEquals(senhaColaborador, senhaRepetidaColaborador)) {
             let data = getFormData();
 
             let response = await signUpEmployee(data);
 
             if (response == 200) {
-                resetFormFields();
+                resetEmployeeFormFields();
                 alert("Colaborador cadastro com sucesso");
             } else {
                 alert("Falha ao cadastrar colaborador");
@@ -46,22 +83,56 @@ const cadastroColaborador = () => {
         }
     }
 
+        //Função para o botão de cadastro do cliente
+        const handleCadastroClienteClick = async () => {
+            if (isEquals(senhaCliente, senhaRepetidaCliente)) {
+                let data = getCustomerFormData();
+    
+                let response = await signUpCustomer(data);
+    
+                if (response == 200) {
+                    resetCustomerFormFields();
+                    alert("Cliente cadastro com sucesso");
+                } else {
+                    alert("Falha ao cadastrar cliente");
+                }
+            }
+        }
+
     const getFormData = () => {
         return {
-            email: email,
-            name: nome,
-            password: senha,
+            email: emailColaborador,
+            name: nomeColaborador,
+            password: senhaColaborador,
             role: "EMPLOYEE",
             departmentName: departamento
         }
     }
 
-    const resetFormFields = () => {
-        setEmail('')
-        setNome('')
-        setSenha('')
-        setSenhaRepetida('')
+    const getCustomerFormData = () => {
+        return {
+            email: emailCliente,
+            name: nomeCliente,
+            password: senhaCliente,
+            role: "CUSTOMER",
+            phone: telefoneCliente
+        }
+    }
+
+    const resetEmployeeFormFields = () => {
+        setEmailColaborador('')
+        setNomeColaborador('')
+        setSenhaColaborador('')
+        setSenhaRepetidaColaborador('')
         setDepartament('')
+    }
+
+    const resetCustomerFormFields = () => {
+        setEmailCliente('')
+        setNomeCliente('')
+        setTelefoneCliente('')
+        setSenhaCliente('')
+        setSenhaRepetidaCliente('')
     }
 
     const [tipoCadastro, setTipoCadastro] = useState('Cadastro de Colaborador');
@@ -79,13 +150,13 @@ const cadastroColaborador = () => {
                     <div className="flex">
                         <div className="flex flex-col w-[45%] mt-5 mb-2">
                             <label className="text-gray-600 text-xl">Nome Completo</label>
-                            <input type="text" id="input-nome" value={nome} onChange={handleNomeChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="text" id="input-nome" value={nomeColaborador} onChange={handleNomeColaboradorChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                             <label className="text-gray-600 text-xl">E-mail</label>
-                            <input type="text" id="input-email" value={email} onChange={handleEmailChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="text" id="input-email" value={emailColaborador} onChange={handleEmailColaboradorChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                             <label className="text-gray-600 text-xl">Senha de Acesso</label>
-                            <input type="password" id="input-senha" value={senha} onChange={handleSenhaChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="password" id="input-senha" value={senhaColaborador} onChange={handleSenhaColaboradorChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                             <label className="text-gray-600 text-xl">Confirme a senha</label>
-                            <input type="password" id="input-senha-repetida" value={senhaRepetida} onChange={handleSenhaRepetidaChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="password" id="input-senha-repetida" value={senhaRepetidaColaborador} onChange={handleSenhaRepetidaColaboradorChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                         </div>
 
                         <div className="flex flex-col w-[45%] mt-5 mb-2 ml-7">
@@ -100,7 +171,7 @@ const cadastroColaborador = () => {
 
                 </section>
 
-                <button className="rounded-full w-[18%] border-none bg-green-700 shadow-lg cursor-pointer text-white text-lg mt-4 ml-[82%] pb-3 pt-3 pl-2 pr-2 hover:shadow-xl active:shadow-md"
+                <button onClick={handleCadastroColaboradorClick} className="rounded-full w-[23%] border-none bg-green-700 shadow-lg cursor-pointer text-white text-lg mt-4 ml-[77%] hover:shadow-xl active:shadow-md"
                 >Cadastrar colaborador</button>
             </div>
         );
@@ -115,23 +186,23 @@ const cadastroColaborador = () => {
                     <div className="flex">
                         <div className="flex flex-col w-[45%] mt-5 mb-2">
                             <label className="text-gray-600 text-xl">Nome Completo</label>
-                            <input type="text" id="input-nome" className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="text" id="input-nome" value={nomeCliente} onChange={handleNomeClienteChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                             <label className="text-gray-600 text-xl">E-mail</label>
-                            <input type="text" id="input-email" className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="text" id="input-email" value={emailCliente} onChange={handleEmailClienteChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                             <label className="text-gray-600 text-xl">Telefone</label>
-                            <input type="text" id="telefone" className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="text" id="telefone" value={telefoneCliente} onChange={handleTelefoneClienteChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                         </div>
 
                         <div className="flex flex-col w-[45%] mt-5 mb-2 ml-7">
                             <label className="text-gray-600 text-xl">Senha de Acesso</label>
-                            <input type="password" id="input-senha" className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="password" id="input-senha" value={senhaCliente} onChange={handleSenhaClienteChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                             <label className="text-gray-600 text-xl">Confirme a senha</label>
-                            <input type="password" id="input-senha-repetida" className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
+                            <input type="password" id="input-senha-repetida" value={senhaRepetidaCliente} onChange={handleSenhaRepetidaClienteChange} className="pl-2 text-gray-600 h-[6vh] text-lg rounded-full border border-green-700 mb-4 outline-none" />
                         </div>
                     </div>
                 </section>
                 
-                    <button className="rounded-full w-[18%] border-none bg-green-700 shadow-lg cursor-pointer text-white text-lg mt-4 ml-[82%] p-3 hover:shadow-xl active:shadow-md"
+                    <button onClick={handleCadastroClienteClick} className="rounded-full w-[18%] border-none bg-green-700 shadow-lg cursor-pointer text-white text-lg mt-4 ml-[82%] p-3 hover:shadow-xl active:shadow-md"
                     >Cadastrar cliente</button>
             </div>
         );
@@ -139,21 +210,18 @@ const cadastroColaborador = () => {
 
     return (
         <div>
-            <NavbarAdm />
+            <Navbar />
 
             <section className="rounded-lg border border-gray-500 bg-white flex flex-col items-left mx-[10%] mt-[5%] p-[3%]">
-                <input
-                    type="text"
+                <select
                     id="input-tipo-cadastro"
-                    list="tipo-cadastro"
                     value={tipoCadastro}
                     onChange={handleTipoCadastroChange}
                     className="pl-2 text-gray-600 h-[6vh] text-4xl rounded-full border border-none mb-4 outline-none"
-                />
-                <datalist id="tipo-cadastro">
-                    <option value="Cadastro de Colaborador" className="text-4xl"></option>
-                    <option value="Cadastro de Cliente" className="text-4xl"></option>
-                </datalist>
+                >
+                    <option value="Cadastro de Colaborador" className="text-xl">Cadastro de Colaborador</option>
+                    <option value="Cadastro de Cliente" className="text-xl">Cadastro de Cliente</option>
+                </select>
             </section>
 
             <section className="mx-[10%] mt-[2%]">
