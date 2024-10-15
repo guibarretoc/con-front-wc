3cca525896ff51c05ee47c0ef948062efb8e8b98
Filipe da Silva Granja <filipe.00000847940@unicap.br>
2024-10-15 19:25:47 -0300

    realizando operações de ajuste do codigo

--- src/components/CreatTicketPage/CreateTicketComponent.jsx
+++ src/components/CreatTicketPage/CreateTicketComponent.jsx
@@ -18,7 +18,7 @@ function CreateTicket() {
   function getCurrentDate() {
     const today = new Date();
     const day = String(today.getDate()).padStart(2, '0');
-    const month = String(today.getMonth() + 1).padStart(2, '0'); // +1 porque os meses começam em 0
+    const month = String(today.getMonth() + 1).padStart(2, '0');
     const year = today.getFullYear();
   
     return `${day}/${month}/${year}`;
